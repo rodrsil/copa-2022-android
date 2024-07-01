2ae0149906a6ee257b941434f821b6d619697148
Rodrigo <rodrigorafael2323@gmail.com>
2024-07-01 17:07:25 -0300

    finishing tasks

--- notification-scheduler/src/main/kotlin/me/dio/copa/catar/notification/scheduler/extensions/NotificationExtensions.kt
+++ notification-scheduler/src/main/kotlin/me/dio/copa/catar/notification/scheduler/extensions/NotificationExtensions.kt
@@ -1,10 +1,13 @@
 package me.dio.copa.catar.notification.scheduler.extensions
 
+import android.Manifest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import me.dio.copa.catar.notification.scheduler.R
@@ -17,6 +20,20 @@ fun Context.showNotification(title: String, content: String) {
     createNotificationChannel()
     val notification = getNotification(title, content)
 
+    if (ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        return
+    }
     NotificationManagerCompat
         .from(this)
         .notify(content.hashCode(), notification)
